a885f2604f66fce88947e9ea7e1bdec385d5aeed
Dexone <dm346@mail.ru>
2024-04-03 20:51:21 +0300

    update

--- pages/index.vue
+++ pages/index.vue
@@ -56,7 +56,7 @@
 
 
 
-          <button v-if="cartStore.simile[main.id] == -1" @click="addToFavourite(main), syncSimile()"
+          <button @click="addToFavourite(main)"
             class="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 "><svg
               class="w-6 h-6 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
               fill="none" viewBox="0 0 24 24">
@@ -65,14 +65,14 @@
             </svg>
           </button>
 
-          <button v-else @click="deleteFavourite(cartStore.simile[main.id]), syncSimile()"
+          <!-- <button 
             class="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 "><svg
               class="w-6 h-6 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
               fill="currentColor" viewBox="0 0 24 24">
               <path
                 d="m12.75 20.66 6.184-7.098c2.677-2.884 2.559-6.506.754-8.705-.898-1.095-2.206-1.816-3.72-1.855-1.293-.034-2.652.43-3.963 1.442-1.315-1.012-2.678-1.476-3.973-1.442-1.515.04-2.825.76-3.724 1.855-1.806 2.201-1.915 5.823.772 8.706l6.183 7.097c.19.216.46.34.743.34a.985.985 0 0 0 .743-.34Z" />
             </svg>
-          </button>
+          </button> -->
         </div>
       </div>
     </div>
@@ -168,5 +168,30 @@ async function addToCart(value) {
   const { data } = await $fetch(`${runtimeConfig.public.apiBase}/cart`, { method: 'POST', body: value })
 
 }
+
+
+
+
+
+
+async function addToFavourite(value){
+  const { data } = await $fetch(`${runtimeConfig.public.apiBase}/favourite`, { method: 'POST', body: value })
+  console.log("test")
+}
+
+
+
+
+
+
+
+
+
+
+
+
 await update()
+
+
+
 </script>
