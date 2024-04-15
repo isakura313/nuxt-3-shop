1b33dbf586c2f09ab52880f612ba9586760ccace
Dexone <dm346@mail.ru>
2024-04-15 16:32:33 +0300

    update cart

--- pages/cart.vue
+++ pages/cart.vue
@@ -22,6 +22,10 @@
             </svg>
           </a>
 
+
+          <a v-if="productStore.quantity[0] == 0">
+            Добавьте что-то скорее
+          </a>
           <a v-else>
             {{ productStore.quantity[1].toLocaleString() }} ₽
           </a>
@@ -61,8 +65,7 @@
                 <a type="button"
                   class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200">
                   <a v-if="productStore.simile[main.id] == 'loader'">
-                    <svg aria-hidden="true"
-                      class="inline w-4 h-4 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                    <svg aria-hidden="true" class="inline w-4 h-4 text-gray-200 animate-spin fill-blue-600"
                       viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <path
                         d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
@@ -83,7 +86,9 @@
                 </button>
 
                 <div class="inline-flex text-xl items-center text-base font-semibold text-gray-900 mr-2">
-                  {{ (main.price * productStore.simile[main.id]).toLocaleString() }} ₽
+                  <a v-if="productStore.simile[main.id] == 'loader'">считаем...</a>
+                  <a v-else> {{ (main.price * productStore.simile[main.id]).toLocaleString() }} ₽</a>
+
                 </div>
               </div>
             </li>
@@ -92,6 +97,7 @@
       </div>
     </div>
   </div>
+
 </template>
 
 
